4d51864dc1db111e773a0809517e2052e24cff20
iRobsie <71240401+iRobsie@users.noreply.github.com>
2023-09-03 11:59:22 -0500

    Create ignore.py

---
+++ ignore.py
@@ -0,0 +1,137 @@
+base_core_address = 0x026E04C0
+
+skyscalegreenbaraddress = 0x10A76EE
+
+griffoninstaboostaddress = 0x11F591B
+
+fishingmasteraddress = 0x10AD848
+
+instafishingaddress = 0x022C22E0
+
+clippingaddress = 0x130D2F9
+
+fullclippingaddress = 0x130D2FD
+
+map_addr = 0x273F1FC
+
+
+
+
+
+
+#==============
+
+mountstamina_base_mem = base_core_address
+mountstamina_offset_mem = [0x38, 0x2D0, 0x18, 0x0, 0x60, 0xC]
+
+mounttype_base_mem = base_core_address
+mounttype_offset_mem = [0x38, 0x2D0, 0x18, 0x0, 0x60, 0x14]
+
+fishing_insta_hook_base_mem = instafishingaddress
+fishing_insta_hook_offset_mem = [0x0, 0x0, 0x60, 0x98, 0x0, 0x20, 0x68]
+
+
+fishing_base_mem = instafishingaddress
+fishing_offset_mem = [0x0, 0x0, 0x60, 0x98, 0x0, 0x20, 0x80]
+
+grafity_base_mem = base_core_address
+grafity_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x294]
+
+wallclimb_base_mem = base_core_address
+wallclimb_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x29C]
+
+speed_base_mem = base_core_address
+speed_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x2B8]
+
+speed_base_mem1 = base_core_address
+speed_offset_mem1 = [0x38, 0x28, 0x80, 0x38, 0x2B4]
+
+speed_base_mem2 = base_core_address
+speed_offset_mem2 = [0x38, 0x28, 0x80, 0x38, 0x2B0]
+
+downrate_glider_base_mem = base_core_address
+downrate_glider_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x2DC]
+
+vypos_base_mem = base_core_address
+vypos_offset_mem = [0x38, 0x28, 0x38, 0x80, 0x38, 0x1A8]
+
+
+ground_base_mem = base_core_address
+ground_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x118]
+
+xpos_base_mem = base_core_address
+xpos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x120]
+
+zpos_base_mem = base_core_address
+zpos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x124]
+
+ypos_base_mem = base_core_address
+ypos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x128]
+#=============
+LWS4_EOD_AND_SOTO_MAP_LIST = [1517, 1509, 1510, 1371, 1015, 1317, 1428, 1422, 1490, 1438, 1442, 1452]
+pvp_pve_map = [350, 549, 1305, 1171, 554, 795, 1163, 900, 894, 875, 95, 96, 1099, 38, 899]
+
+versionnow = "1.0.0.1"
+
+
+
+mountmasterupdatex = "Last Update : September 03, 2023"
+
+messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
+
+donator = """
+==||== \n 
+LEGENDARY COMMANDER : \n
+-K W, -I P, -H W, \n
+\n\n
+ASCENDED COMMANDER : \n
+- R Z , - N M S, - N H, -M D B, -J S, -J A, - F C
+\n -V R, -M D G, -R H, -P L C, -F O X, -S N, -O Y I, -W G, - E S
+\n -M Q, -R D, -B K, N K, -T T, -A C, - R P,  -I V \n\n\n
+============================\n
+that name displayed is the donator\n
+if you donator and want specific nickname, just let me know\n
+consider donating to keep this project alive\n
+thanks for your support !!!
+"""
+#=============
+#=============
+#=============
+
+uuid_saved = ["f89f95cbcc","da698b0e4c","d7e53d75f2","3496ec8c63","e2823149be","01035bf5b7","e84e400c61","9e87fbea81","33bfcbbadc","d35d2ec041","2bd4280a7d","53b625f3d9","df33e92ff7","x5eb7799781","090afbe976","7e10425e1b","2ffeb1091c","d6fbdc1520","0f407ed34e","5debc04d27","bc9c7d9eb2","da2569d097","30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","2fda4ec1b0","d72f0e4f08"]
+
+'''
+f89f95cbcc = ivicask
+da698b0e4c = RarePepe
+d7e53d75f2 = Aiden Cross 
+3496ec8c63 = tiredtbh
+e2823149be = Naksu
+01035bf5b7 = Beutelkramer
+e84e400c61 = ridicul0us
+68085ef0f0 = Morqitta old
+9e87fbea81 = Morqitta new
+33bfcbbadc = EddieSmurphy
+d35d2ec041 = wongo
+2bd4280a7d = Truckingsteve#007
+df33e92ff7 =  OD YR INK
+5eb7799781 = Syrix == not confirmed, i add x on the id
+090afbe976 = Sinic
+7e10425e1b = フォックスマン
+2ffeb1091c = polychromatik
+d6fbdc1520 = Reeeehaww
+0f407ed34e = Mindaugas
+5debc04d27 = Hawaiian
+da2569d097 = Varek
+30fac06e50 = falco
+6fe74d85f3 = Jaxx Armstrong
+b275bc6551 53b625f3d9 = don't mind me
+d05112dc9e = morbiddarkblood
+cf80d1275b = NtajHmoob
+f4437fc8fe = nemesis
+3678bde5e3 = intel_power
+bc9c7d9eb2 = sabay
+2fda4ec1b0 = kowa
+d72f0e4f08 = rietzee
+'''
+#=============
+#==============
